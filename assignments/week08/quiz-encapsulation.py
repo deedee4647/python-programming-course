c13a0ee9a90ead6ad17b914a3c74998d78316078
RATCHAPHOL THAMKOWITTAKUL <ratchaphol.tha@ku.th>
2025-09-16 15:08:02 +0700

    789

--- assignments/week08/quiz-encapsulation.py
+++ assignments/week08/quiz-encapsulation.py
@@ -1,9 +1,24 @@
-"""
-Write a Python class Rectangle with:
+class Rectangle:
 
-Private attributes for length and width
-Methods to calculate area (getArea()) and perimeter getPerimeter())
-A method to check if it's a square (isSquare())
+    def __init__(self,length,width):
+        self.__length = length
+        self.__width = width
 
-"""
-    
+    def getArea(self,):
+        area = self.__length * self.__width
+        return f"Area = {area}"
+
+    def getPerimeter(self,):
+        perimeter = 2 * (self.__length + self.__width)
+        return f"Perimeter = {perimeter}"
+        
+    def isSquare(self):
+        if self.__length == self.__width:
+            return f"This rectangle is square"
+        else:
+            return f"This rectangle is not square"
+        
+rectangle = Rectangle(10,5)
+print(rectangle.getArea())
+rectangle.getPerimeter()
+print(rectangle.isSquare())
